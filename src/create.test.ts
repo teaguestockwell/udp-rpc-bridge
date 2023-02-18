fe3df8f746b7822bc61f738dd015169811629ab2
teagueStockwell <tsAppDevelopment@gmail.com>
2023-02-17 22:26:28 -0800

    WIP more api shaping

--- src/create.test.ts
+++ src/create.test.ts
@@ -2,39 +2,46 @@ import { create } from './create';
 
 describe('create', () => {
   it('it can produce a client', () => {
-    type Events = {
-      putComment: [
-        { id?: string; text: string },
-        {
-          204: {};
-          201: { id: string; text: string };
-          400: { msg: string };
-        }
-      ];
-    };
+    type RPC = {
 
-    const store: Record<string, any> = {};
-    const client = create<Events>((api) => ({
-      putComment: async (data) => {
-        // validation error
-        if (data.id && !data.id.trim()) {
-          return { msg: 'no id' };
-        }
+    }
 
-        // update
-        if (data.id) {
-          store[data.id] = data;
-          return { status: 200, data };
-        }
 
-        const res = await api('putComment', {id: '1', text: 'a'})
-
-        // create
-        const id = Date.now() + '';
-        store[id] = { data };
-        return { status: 200, data: { id, ...data } };
+    const client = create(({ call, set, get }) => ({
+      rpc: {
+        putComment: async (data: number) => {},
+      },
+      state: {
+        commentValue: '',
+      },
+      actions: {
+        onCommentChange: (e: any) => {
+          set({ commentId: '' });
+        },
       },
     }));
+
+    // const client = create((api) => ({
+    //   putComment: async (data) => {
+    //     // validation error
+    //     if (data.id && !data.id.trim()) {
+    //       return { msg: 'no id' };
+    //     }
+
+    //     // update
+    //     if (data.id) {
+    //       store[data.id] = data;
+    //       return { status: 200, data };
+    //     }
+
+    //     const res = await api('putComment', {id: '1', text: 'a'})
+
+    //     // create
+    //     const id = Date.now() + '';
+    //     store[id] = { data };
+    //     return { status: 200, data: { id, ...data } };
+    //   },
+    // }));
 
     expect(client).toBeTruthy();
   });
--- src/create.ts
+++ src/create.ts
@@ -1,16 +1,21 @@
+type Meta = { callerId: string; requestId: string };
+
 export const create = <
-  Events extends { [rpc: string]: [Arg, Record<number, object>] },
-  Arg = object,
-  ReqMeta = { requestId: string; requestorId: string },
-  Handlers = {
-    [RPC in keyof Events]: (
-      arg: Events[RPC][0] & ReqMeta
-    ) => Promise<Events[RPC][1][number]>;
+  Rpc extends {
+    [rpc: string]: (data: any) => Promise<any>;
+  },
+  State,
+  Actions extends {
+    [name: string]: (arg?: any) => any;
   }
 >(
-  produce: <RPC extends keyof Events>(
-    get: <RPC extends keyof Events>(rpc: RPC,req: Events[RPC][0]) => Promise<Events[RPC][1][number]>
-  ) => Handlers
-) => {
-
-};
+  produce: <RPC extends keyof Rpc>(api: {
+    call: (rpc: RPC, data: Parameters<Rpc[RPC]>) => ReturnType<Rpc[RPC]>;
+    set: (next: Partial<State> | ((prev: State) => Partial<State>)) => void;
+    get: () => State;
+  }) => {
+    rpc: Rpc;
+    state?: State;
+    actions?: Actions;
+  }
+) => {};
